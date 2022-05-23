15cc8a977a1c404d890ebc4f9cfa215bd10bda21
jacobg1 <greenwald.j8@gmail.com>
2022-05-22 20:47:41 -0400

    Moving header and components into own folder

---
+++ src/components/header/mobile-header.tsx
@@ -0,0 +1,47 @@
+import { default as React } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import { SiteLinks } from "../navigation";
+import type { SxProps } from "@mui/material";
+import { MobileHeaderProps } from "../../types/interface";
+
+const mobileHeaderStyles: SxProps = {
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  padding: "30px 20px 20px",
+  alignItems: "center",
+  backgroundColor: "#f5f5f5",
+  "& .link-holder": {
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    height: "30%",
+  },
+  svg: {
+    width: "30px",
+    height: "30px",
+  },
+};
+
+export const MobileHeader = ({
+  navLinks,
+  isOpen,
+  onClick,
+}: MobileHeaderProps): JSX.Element => {
+  return (
+    <Drawer anchor="right" open={isOpen} onClose={onClick}>
+      <Box sx={mobileHeaderStyles} className="mobile-header">
+        <HighlightOffIcon onClick={onClick} />
+        <Box className="link-holder">
+          <SiteLinks navLinks={navLinks} />
+        </Box>
+        <Typography variant="body1">greenwald.j8@gmail.com</Typography>
+      </Box>
+    </Drawer>
+  );
+};
--- src/components/header/site-header.tsx
+++ src/components/header/site-header.tsx
@@ -1,32 +1,22 @@
 import { default as React, useState } from "react";
+import type { SxProps } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import Drawer from "@mui/material/Drawer";
 import DensityMediumSharpIcon from "@mui/icons-material/DensityMediumSharp";
-import HighlightOffIcon from "@mui/icons-material/HighlightOff";
-import { SiteLinks } from "./navigation";
-import { useNavLinksQuery } from "../hooks/useNavLinksQuery";
-import type { SxProps } from "@mui/material";
-import {
-  NavLinkItem,
-  NavLinkList,
-  MobileHeaderProps,
-} from "../types/interface";
+import { useNavLinksQuery } from "../../hooks/useNavLinksQuery";
+import { MobileHeader } from "./mobile-header";
+import { DesktopHeader } from "./desktop-header";
+import { NavLinkItem } from "../../types/interface";
 
 const appBarStyles: SxProps = {
-  // backgroundColor: "#dddbd9",
   backgroundColor: "#ffffff",
   display: "flex",
   flexDirection: "row",
   justifyContent: "space-between",
   padding: "17px 17px",
   borderBottom: "2px solid #0572e1",
-  "& .mobile-header, .desktop-header": {
-    // display: "flex",
-    // justifyContent: "space-around",
-  },
   "& .desktop-header": {
     display: { xs: "none", sm: "flex" },
   },
@@ -36,12 +26,10 @@ const appBarStyles: SxProps = {
   },
   "& .MuiTypography-body1": {
     paddingTop: "5px",
-    // color: "#007eff",
   },
   "& .MuiIconButton-root": {
     color: "#000000",
   },
-  // "& .MuiTabs-indicator": {},
 };
 
 export const SiteHeader = (): JSX.Element => {
@@ -72,52 +60,5 @@ export const SiteHeader = (): JSX.Element => {
         <DensityMediumSharpIcon fontSize="medium" />
       </IconButton>
     </AppBar>
-  );
-};
-
-const DesktopHeader = ({ navLinks }: NavLinkList): JSX.Element => {
-  return (
-    <Box className="desktop-header">
-      <SiteLinks navLinks={navLinks} />
-    </Box>
-  );
-};
-
-const mobileHeaderStyles: SxProps = {
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  padding: "30px 20px 20px",
-  alignItems: "center",
-  backgroundColor: "#f5f5f5",
-  "& .link-holder": {
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
-    height: "30%",
-  },
-  svg: {
-    width: "30px",
-    height: "30px",
-  },
-};
-
-const MobileHeader = ({
-  navLinks,
-  isOpen,
-  onClick,
-}: MobileHeaderProps): JSX.Element => {
-  return (
-    <Drawer anchor="right" open={isOpen} onClose={onClick}>
-      <Box sx={mobileHeaderStyles} className="mobile-header">
-        <HighlightOffIcon onClick={onClick} />
-        <Box className="link-holder">
-          <SiteLinks navLinks={navLinks} />
-        </Box>
-        <Typography variant="body1">greenwald.j8@gmail.com</Typography>
-      </Box>
-    </Drawer>
   );
 };
