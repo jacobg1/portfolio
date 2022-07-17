b611e12b38dd88a271819577286105d4b22599df
jacobg1 <greenwald.j8@gmail.com>
2022-07-16 20:23:20 -0400

    Update name of hook

---
+++ src/hooks/use-metadata-query.ts
@@ -0,0 +1,24 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+import { MetadataQuery } from "../types/interface";
+
+const query = graphql`
+  query PageMeta {
+    site {
+      siteMetadata {
+        title
+        description
+        keywords
+        siteUrl
+      }
+    }
+  }
+`;
+
+export function useMetadataQuery<T>(): T {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery<MetadataQuery<T>>(query);
+
+  return siteMetadata;
+}
