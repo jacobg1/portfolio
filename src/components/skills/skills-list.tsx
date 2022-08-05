933e11b7797a40b62902783f1e917e62fc0e7965
jacobg1 <greenwald.j8@gmail.com>
2022-08-05 16:31:03 -0400

    Update skills list styles

--- src/components/skills/skills-list.tsx
+++ src/components/skills/skills-list.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import type { SxProps } from "@mui/material";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 import { useSkillsQuery } from "../../hooks/use-skills-query";
 import { Skill } from "../../types/interface";
@@ -10,8 +11,6 @@ import { SingleSkill } from "./single-skill";
 const skillsListStyles: SxProps = {
   display: "flex",
   flexWrap: "wrap",
-  maxWidth: "1048px",
-  margin: "60px auto",
   "& .button-link-container": {
     width: "auto",
     margin: "5px",
@@ -19,17 +18,43 @@ const skillsListStyles: SxProps = {
       textTransform: "lowercase",
       border: "1px solid #1976d2",
       boxShadow: "1px 1px 1px black",
+      "&:hover": {
+        background: "white",
+        boxShadow: "none",
+        borderColor: "black",
+      },
     },
+  },
+};
+
+const skillsSectionStyles: SxProps = {
+  margin: "30px auto",
+  "& .skills-header": {
+    lineHeight: "24px",
+  },
+  "& .robot-arm": {
+    padding: "0 0 5px 5px",
+    fontSize: { xs: "18px", sm: "22px", lg: "24px" },
   },
 };
 
 export const SkillsList = (): JSX.Element => {
   const mySkills = useSkillsQuery<Skill[]>();
   return (
-    <Box sx={skillsListStyles}>
-      {mySkills.map((skill, i) => (
-        <SingleSkill key={`skill-${i}`} {...skill} />
-      ))}
+    <Box sx={skillsSectionStyles}>
+      <Box display="flex" alignItems="center">
+        <Typography className="skills-header" variant="h3">
+          Skills
+        </Typography>
+        <Box component="span" className="robot-arm">
+          🦾
+        </Box>
+      </Box>
+      <Box sx={skillsListStyles}>
+        {mySkills.map((skill, i) => (
+          <SingleSkill key={`skill-${i}`} {...skill} />
+        ))}
+      </Box>
     </Box>
   );
 };
