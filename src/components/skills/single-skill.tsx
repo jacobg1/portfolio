f7c671e6c0e1fb7bf49bf7841c305fb29fc7a1d4
jacobg1 <greenwald.j8@gmail.com>
2022-08-06 15:39:35 -0400

    Moving mapping to own file

--- src/components/skills/single-skill.tsx
+++ src/components/skills/single-skill.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
-import StorageIcon from "@mui/icons-material/Storage";
-import TerminalIcon from "@mui/icons-material/Terminal";
-import WebIcon from "@mui/icons-material/Web";
 import type { SxProps } from "@mui/material";
 import Box from "@mui/material/Box";
 
 import { Skill, SkillsEnum } from "../../types/interface";
 import { ButtonLink } from "../global/button-link";
+import { skillIconsMapping } from "./skill-icons-mapping";
 
 const skillIconStyles: SxProps = {
   alignSelf: "center",
@@ -15,23 +13,9 @@ const skillIconStyles: SxProps = {
   svg: {
     width: "auto",
   },
-  "&.database svg": {
-    fill: "#009688",
-  },
-  "&.frontend svg": {
-    fill: "#0900ff",
-  },
-  "&.backend svg": {
-    fill: "#0900ff",
-  },
 };
 
 export const SingleSkill = ({ text, type, website }: Skill): JSX.Element => {
-  const skillIconsMapping = {
-    [SkillsEnum.BACKEND]: <TerminalIcon />,
-    [SkillsEnum.FRONTEND]: <WebIcon />,
-    [SkillsEnum.DATABASE]: <StorageIcon />,
-  };
   const getSkillIcons: JSX.Element[] = type
     .split(",")
     .map((type: string, i: number) => {
