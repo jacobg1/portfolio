276339a2dd30120da7f2ec66ccef52fafb10d3c3
jacobg1 <greenwald.j8@gmail.com>
2024-07-01 12:41:25 -0400

    wrap mobile header with custom mui component

--- src/components/header/mobile-header.tsx
+++ src/components/header/mobile-header.tsx
@@ -5,28 +5,19 @@ import type { SxProps } from "@mui/material";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Link from "@mui/material/Link";
+import { styled } from "@mui/system";
 import { globalHistory } from "@reach/router";
 
 import { MobileHeaderProps } from "../../types/interface";
 import { SiteLinks } from "../global/navigation";
 
 const mobileHeaderStyles: SxProps = {
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  padding: "30px 20px 20px",
-  alignItems: "center",
-  backgroundColor: "#f5f5f5",
   "& .link-holder": {
     textAlign: "center",
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-around",
     height: "30%",
-    a: {
-      color: "#0572e1",
-    },
   },
   svg: {
     width: "30px",
@@ -34,6 +25,13 @@ const mobileHeaderStyles: SxProps = {
     cursor: "pointer",
   },
 };
+
+const MobileHeaderContainer = styled("div", {
+  name: "MuiMobileHeaderContainer",
+  overridesResolver: (_, styles) => {
+    return styles.root;
+  },
+})``;
 
 export const MobileHeader = ({
   navLinks,
@@ -47,13 +45,13 @@ export const MobileHeader = ({
   }, [onClick]);
   return (
     <Drawer anchor="right" open={isOpen} onClose={onClick}>
-      <Box sx={mobileHeaderStyles} className="mobile-header">
+      <MobileHeaderContainer sx={mobileHeaderStyles}>
         <HighlightOffIcon onClick={onClick} />
         <Box className="link-holder">
           <SiteLinks navLinks={navLinks} />
         </Box>
         <Link href="mailto:greenwald.j8@gmail.com">greenwald.j8@gmail.com</Link>
-      </Box>
+      </MobileHeaderContainer>
     </Drawer>
   );
 };
