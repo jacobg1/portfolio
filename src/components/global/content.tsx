81e7ab0157f9848a71e4d2d776aa8ff91e6341fb
jacobg1 <greenwald.j8@gmail.com>
2024-09-02 18:53:52 -0400

    add dompurify package

--- src/components/global/content.tsx
+++ src/components/global/content.tsx
@@ -1,14 +1,21 @@
 import * as React from "react";
 
+import DOMPurify from "isomorphic-dompurify";
+
 import type { HtmlString } from "../../types";
 
-type ContentProps = { content: HtmlString };
+type ContentProps = {
+  content: HtmlString;
+  className?: string;
+};
 
-export const Content = ({ content }: ContentProps): JSX.Element => {
+export const Content = ({ content, className }: ContentProps): JSX.Element => {
   return (
     <div
-      className="page-content"
-      dangerouslySetInnerHTML={{ __html: content }}
+      className={className || "page-content"}
+      dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(content, { ADD_ATTR: ["target"] }),
+      }}
     />
   );
 };
